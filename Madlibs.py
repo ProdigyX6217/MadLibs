eb6a1a9b8949d9ca10c4a2c9c11d8cd4096d0c38
Student Laptop_7/19_1 <studentlaptop_719_1@Students-MacBook-Pro.local>
2019-09-02 17:54:05 -0700

    Created Story

---
+++ Madlibs.py
@@ -0,0 +1,13 @@
+# http://i.infopls.com/fe/pc/0,,18040-1401,00.pdf
+
+print ("{adverb}, my entire family went to the beach one day.")
+print ("They {verb} their towels and layed on the {adjective} beach for a while.")
+print ("When it starts to feel {adjective}, We move our belongings elsewhere.")
+print ("Then, my youngest sister takes my shovel and begins building a {noun}.")
+print ("She runs to the edge of the shore where the water is at the most {adjective}.")
+print ("My father {adverb} tells me she's too young to go in the water.")
+print ("I sprint towards her when a {noun} blindsides me.")
+print ("When the tide {verb}, my baby sister is nowhere to be found.")
+print ("Panicked, my {adjective} family scans the beachfront.")
+print ("Mom and Dad {adverb} jump into the {adjective} water to find my missing baby sister.")
+print ("Just then, I see a shovel in the water and my sister smiling at us.")

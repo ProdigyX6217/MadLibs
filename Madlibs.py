4d4f1fd7836dca9083ed65814cfbfcd8a9830543
Student Laptop_7/19_1 <studentlaptop_719_1@Students-MacBook-Pro.local>
2019-09-02 18:39:11 -0700

    Defined Functions

--- Madlibs.py
+++ Madlibs.py
@@ -1,22 +1,32 @@
 # http://i.infopls.com/fe/pc/0,,18040-1401,00.pdf
-adverb = input("Enter an Adverb: ")
-verb = input("Enter a Verb: ")
-adjective = input("Enter an Adjective: ")
-noun = input("Enter a Noun: ")
+def adverb():
+    input("Enter an Adverb: ")
+    return
 
+def verb():
+    input("Enter a Verb: ")
+    return
+
+def adjective():
+    input("Enter an Adjective: ")
+    return
+
+def noun():
+    input("Enter a Noun: ")
+    return
 
 def user_input(prompt):
     user_input = input(prompt)
     return user_input
 
-print ("{adverb}, my entire family went to the beach one day.")
-print ("They {verb} their towels and layed on the {adjective} beach for a while.")
-print ("When it starts to feel {adjective}, We move our belongings elsewhere.")
-print ("Then, my youngest sister takes my shovel and begins building a {noun}.")
-print ("She runs to the edge of the shore where the water is at the most {adjective}.")
-print ("My father {adverb} tells me she's too young to go in the water.")
-print ("I sprint towards her when a {noun} blindsides me.")
-print ("When the tide {verb}, my baby sister is nowhere to be found.")
-print ("Panicked, my {adjective} family scans the beachfront.")
-print ("Mom and Dad {adverb} jump into the {adjective} water to find my missing baby sister.")
-print ("Just then, I see a shovel in the water and my sister smiling at us.")
+print (f"""{adverb}, my entire family went to the beach one day.
+ They {verb} their towels and layed on the {adjective} beach for a while.
+When it starts to feel {adjective}, We move our belongings elsewhere.
+ Then, my youngest sister takes my shovel and begins building a {noun}.
+She runs to the edge of the shore where the water is at the most {adjective}.
+My father {adverb} tells me she's too young to go in the water.
+ I sprint towards her when a {noun} blindsides me.
+ When the tide {verb}, my baby sister is nowhere to be found.
+Panicked, my {adjective} family scans the beachfront.
+Mom and Dad {adverb} jump into the {adjective} water to find my missing baby sister.
+Just then, I see a shovel in the water and my sister smiling at us.""")
